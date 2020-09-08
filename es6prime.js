8a4a6dad25838de7378a7f6d10709cbc7b6de146
marqui5 <marquisv587@gmail.com>
2020-09-08 14:31:27 +0800

    es6

--- es6prime.js
+++ es6prime.js
@@ -3,6 +3,7 @@ function es6Prime(n) {
 	a[1] = 0; // 1 is not prime
 	var rt = Math.sqrt(n); // calculate only once
 	for (i=2;i<=rt;i++){
+		if(!a[i]) continue
 		for (var j=i*i; j<=n; j+=i) a[j]=0;
 	}
 	return a.filter(Number).toString(); // kick out the zeroes
